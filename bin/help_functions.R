7e0a1e1f92668c951b0eed5f2bbe79d4fb9876f2
vildeka <vilde.kaldhusdal@gmail.com>
2022-06-16 16:31:51 +0200

    initial commit

---
+++ bin/help_functions.R
@@ -0,0 +1,43 @@
+# line tool 
+bez2poly <- function(x){
+  x <- strsplit( gsub("[ ].*[ ]","l",x) ,split = "[a-zA-Z]")[[1]][-1]
+  x <- as.data.frame(strsplit(x,split = ","))
+  x <- apply(x,1,as.numeric)
+  x <- cbind(cumsum(x[,1]), cumsum(x[,2]) )
+  x <- rbind(x,x[1,])
+  return(x)
+}
+
+tmat <- function(x,n=3){
+  tmp <- matrix(0,3,3)
+  x <- as.numeric(strsplit(gsub("[a-z()]","",x),split = ",")[[1]])
+  tmp[c(1,2),c(1,1)] <- x[1:2]
+  tmp[c(1,2),c(2,2)] <- x[3:4]
+  tmp[c(1,2),c(3,3)] <- x[5:6]
+  tmp[3,3] <- 1
+  return(tmp)
+}
+
+rec2poly <- function(x){
+  x <- attributes(x)
+  x <- setNames( as.numeric(unlist(x)[c("x","y","width","height")]) , c("x","y","width","height") )
+  df <- data.frame(
+    x = x["x"] + x["width"] * c(0,0,1,1,0),
+    y = x["y"] + x["height"] * c(0,1,1,0,0))
+  return(df)
+}
+
+get_shape <- function(x){
+  xx <- attributes(x)
+  if( "d" %in% names(xx) ){
+    return( bez2poly( attr(x,"d") ) )
+  } else if( "width"  %in% names(xx) ){
+    return(rec2poly( x ))
+  } 
+}
+
+poly_area <- function(df){
+  res <- t(df)
+  x <- abs( sum(  ((res[1,] * c(res[2,-1],res[2,1])) - (res[2,] * c(res[1,-1],res[1,1]) )) )/2 )
+  return(x)
+}
---
+++ bin/spatial_visualization.R
@@ -0,0 +1,203 @@
+#################
+# GEOM SPATIAL #
+################
+geom_spatial <-  function(mapping = NULL,
+                          data = NULL,
+                          stat = "identity",
+                          position = "identity",
+                          na.rm = FALSE,
+                          show.legend = NA,
+                          inherit.aes = FALSE,
+                          ...) {
+  
+  GeomCustom <- ggproto(
+    "GeomCustom",
+    Geom,
+    setup_data = function(self, data, params) {
+      data <- ggproto_parent(Geom, self)$setup_data(data, params)
+      data
+    },
+    
+    draw_group = function(data, panel_scales, coord) {
+      vp <- grid::viewport(x=data$x, y=data$y)
+      g <- grid::editGrob(data$grob[[1]], vp=vp)
+      ggplot2:::ggname("geom_spatial", g)
+    },
+    
+    required_aes = c("grob","x","y")
+    
+  )
+  
+  layer(
+    geom = GeomCustom,
+    mapping = mapping,
+    data = data,
+    stat = stat,
+    position = position,
+    show.legend = show.legend,
+    inherit.aes = inherit.aes,
+    params = list(na.rm = na.rm, ...)
+  )
+}
+
+#####################
+# PLOTTING FUNCTION #
+#####################
+# spe <- DATA
+# geneid <- sym("CDH1")
+# zoom <- sym("tissue")
+
+vis_gene_p <- function(
+  spe,
+  sampleid = c("P080"),
+  spatial=TRUE,
+  geneid="CDH1", #"LINC01135",# "nFeature_RNA"
+  title=" ",
+  spectral= TRUE,
+  image_id = "hires",
+  alpha = .7,
+  cont_colors = c("lightgray", "mistyrose", "red", "dark red", "black"),
+  point_size = 1.75,
+  img_alpha = .5,
+  zoom = NULL ) {
+  
+  if (!(as_label(geneid) %in% colnames(spe@meta.data))) {
+    spe <- spe %>%
+      mutate(., FetchData(., vars = c(geneid)) ) 
+  }
+  
+  # Colour pallets:
+  if (spectral){
+    myPalette <- colorRampPalette(rev(brewer.pal(11, "Spectral")))
+    cont_colors <- myPalette(100)
+    colour_pallet <- scale_fill_gradientn(colours = cont_colors)
+  }
+  if (is.character(pull(spe, geneid))){
+    # scales::show_col(disc_colors)
+    disc_colors <- c(RColorBrewer::brewer.pal(9,"Pastel1"),
+                     RColorBrewer::brewer.pal(9,"Set1"),
+                     scales::hue_pal()(8),
+                     RColorBrewer::brewer.pal(8,"Set2"),
+                     RColorBrewer::brewer.pal(8,"Accent"),
+                     RColorBrewer::brewer.pal(8,"Pastel2") )
+    colour_pallet <- scale_fill_manual(values = disc_colors)
+  }
+  
+  # filter samples:
+  spe <- spe %>% filter(orig.ident %in% sampleid)
+  # get scale factor:
+  scale_fact <- spe@images[[sampleid]]@scale.factors[[image_id]]
+  geneid <- enquo(geneid)
+  
+  df <- spe@images[[sampleid]]@coordinates %>%
+    mutate(imagecol = .$imagecol * scale_fact) %>%
+    mutate(imagerow = .$imagerow * scale_fact) %>%
+    cbind(.,as_tibble(select(spe, !!(geneid)))) %>%
+    rownames_to_column(var = "barcode") %>%
+    as_tibble() 
+  
+  # set image alpha:
+  im <- spe@images[[sampleid]]@image
+  spe@images[[sampleid]]@image <- matrix(
+    rgb(im[,,1],im[,,2],im[,,3], im[4,,]* img_alpha), nrow=dim(im)[1]) 
+  
+  img <- GetImage(spe, image = sampleid, mode = "raster")
+  
+  # select viewframe:
+  if (!(is.null(zoom))) {
+    l <- df %>% 
+      filter(.data[[zoom]] == 1) %>% 
+      #select(row=imagerow)
+      rename("_row"=imagerow, "_col"=imagecol) %>%
+      summarise(across("_row":"_col", 
+                       list(min=min, max=max), 
+                       .names = "{.fn}{.col}")) 
+  }else{l <- tibble( min_col = 0, max_col = ncol(img),
+                     min_row = 0, max_row = nrow(img))}
+  img_ <- img[l$min_row:l$max_row,l$min_col:l$max_col]
+  
+  # get grob and save as list
+  grob <- grid::rasterGrob(img_, width=unit(1,"npc"), height=unit(1,"npc"))
+  images_tibble <- tibble(sample=factor(sampleid), grob=list(grob))
+  
+  p <- ggplot()+
+    geom_spatial(data=images_tibble, aes(grob=grob), x=0.5, y=0.5)+
+    geom_point(data=df, aes(x=imagecol,y=imagerow,fill=.data[[geneid]]),
+               shape = 21, #colour = "black",
+               size = point_size, stroke = 0.5, alpha = alpha)+
+    geom_path(data = spe@tools[[sampleid]], aes(x=x, y=y, group = interaction(elem_idx)))+
+    colour_pallet +
+    coord_cartesian(expand=FALSE )+
+    
+    xlim(l$min_col,l$max_col) +
+    ylim(l$max_row,l$min_row)
+  
+  p <- p +
+    xlab("") +
+    ylab("") +
+    ggtitle(sampleid)+
+    #labs(fill = "Total UMI")+
+    theme_set(theme_bw(base_size = 10))+
+    theme(panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.background = element_blank(),
+          axis.line = element_blank(),
+          axis.text = element_blank(),
+          axis.ticks = element_blank())
+  return(p)
+}
+
+#################
+# PATH FUNCTIONS #
+#################
+c_curve<-function(start, c1, c2, end, n=100) {
+  c1 <- start+c1
+  c2 <- start+c2
+  end <- start+end
+  sapply((0:n)/n, function(i) {
+    q1 <- start+i*(c1-start)
+    q2 <- c1+i*(c2-c1)
+    q3 <- c2+i*(end-c2)
+    r1 <- q1+i*(q2-q1)
+    r2 <- q2+i*(q3-q2)
+    r1+i*(r2-r1)
+  })
+}
+# https://stackoverflow.com/questions/46221703/converting-svg-to-ggplot
+parse_svg_path <- function(path) {
+  parts <- regmatches(path, gregexpr("[A-Za-z]|,|-?[0-9.]+", path, perl=T))[[1]] 
+  parts <- parts[parts!=","]
+  vals <- suppressWarnings(as.numeric(parts))
+  i <- 1
+  points <- matrix(ncol=0, nrow=2)
+  while(i < length(parts)) {
+    if (parts[i]=="M") {
+      points <- cbind(points, c(vals[i+1], vals[i+2]))
+      i <- i+3
+    } else if (parts[i]=="l") {
+      cpoints <- c_curve(
+        points[, ncol(points)],
+        c(vals[i+1], vals[i+2]),
+        c(vals[i+3], vals[i+4]),
+        c(vals[i+5], vals[i+6])
+      )
+      points <- cbind(points, cpoints)
+      i <- i+7
+    } else {
+      stop(paste("unrecognized command", parts[i]))
+    }
+  }
+  points
+}
+
+#################
+# SP ANNOTATION #
+#################
+# poly <- df %>%
+#   filter(.data[["epi_1"]] == 1)
+# img_path <- paste0( input_dir,"/",red,"/",red,".svg")
+# poly_df <- svgparser::read_svg(img_path, obj_type = 'data.frame')
+
+# ggplot(poly_df) + 
+# geom_path(aes(x, y, colour = name, group = interaction(elem_idx, path_idx))) +
+# scale_y_reverse()
