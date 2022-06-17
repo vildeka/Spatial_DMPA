bc7d8a39f45929272b4572ef5d3971cf1b2ab41f
vildeka <vilde.kaldhusdal@gmail.com>
2022-06-17 16:27:53 +0200

    update functionality

--- bin/spatial_visualization.R
+++ bin/spatial_visualization.R
@@ -47,31 +47,33 @@ geom_spatial <-  function(mapping = NULL,
 # geneid <- sym("CDH1")
 # zoom <- sym("tissue")
 
-vis_gene_p <- function(
+plot_spatial.fun <- function(
   spe,
+  sp_annot = TRUE,
   sampleid = c("P080"),
-  spatial=TRUE,
-  geneid="CDH1", #"LINC01135",# "nFeature_RNA"
-  title=" ",
-  spectral= TRUE,
+  geneid = "CDH1", #"LINC01135",# "nFeature_RNA"
+  title = " ",
   image_id = "hires",
-  alpha = .7,
-  cont_colors = c("lightgray", "mistyrose", "red", "dark red", "black"),
+  alpha = 1,
+  spectral = TRUE,
+  cont_colors = c("grey90", "mistyrose", "red", "dark red", "black"), # lightgray
   point_size = 1.75,
   img_alpha = .5,
   zoom = NULL ) {
   
+  # filter samples:
+  spe <- spe %>% filter(orig.ident %in% sampleid)
+  ## get feature to plot:
   if (!(as_label(geneid) %in% colnames(spe@meta.data))) {
     spe <- spe %>%
       mutate(., FetchData(., vars = c(geneid)) ) 
   }
   
-  # Colour pallets:
+  ## Colour pallets:
   if (spectral){
     myPalette <- colorRampPalette(rev(brewer.pal(11, "Spectral")))
-    cont_colors <- myPalette(100)
-    colour_pallet <- scale_fill_gradientn(colours = cont_colors)
-  }
+    cont_colors <- myPalette(100)}
+  colour_pallet <- scale_fill_gradientn(colours = cont_colors)
   if (is.character(pull(spe, geneid))){
     # scales::show_col(disc_colors)
     disc_colors <- c(RColorBrewer::brewer.pal(9,"Pastel1"),
@@ -79,16 +81,16 @@ vis_gene_p <- function(
                      scales::hue_pal()(8),
                      RColorBrewer::brewer.pal(8,"Set2"),
                      RColorBrewer::brewer.pal(8,"Accent"),
+                     
                      RColorBrewer::brewer.pal(8,"Pastel2") )
     colour_pallet <- scale_fill_manual(values = disc_colors)
   }
   
-  # filter samples:
-  spe <- spe %>% filter(orig.ident %in% sampleid)
   # get scale factor:
   scale_fact <- spe@images[[sampleid]]@scale.factors[[image_id]]
   geneid <- enquo(geneid)
   
+  # get spot coordinates:
   df <- spe@images[[sampleid]]@coordinates %>%
     mutate(imagecol = .$imagecol * scale_fact) %>%
     mutate(imagerow = .$imagerow * scale_fact) %>%
@@ -96,39 +98,54 @@ vis_gene_p <- function(
     rownames_to_column(var = "barcode") %>%
     as_tibble() 
   
-  # set image alpha:
-  im <- spe@images[[sampleid]]@image
-  spe@images[[sampleid]]@image <- matrix(
-    rgb(im[,,1],im[,,2],im[,,3], im[4,,]* img_alpha), nrow=dim(im)[1]) 
-  
-  img <- GetImage(spe, image = sampleid, mode = "raster")
-  
   # select viewframe:
   if (!(is.null(zoom))) {
-    l <- df %>% 
-      filter(.data[[zoom]] == 1) %>% 
+    l <- DATA@tools[[sampleid]] %>% 
+      filter(.data[["name"]] == zoom) %>% # zoom <- "zoom"
       #select(row=imagerow)
-      rename("_row"=imagerow, "_col"=imagecol) %>%
+      rename("_row"=y, "_col"=x) %>%
       summarise(across("_row":"_col", 
                        list(min=min, max=max), 
                        .names = "{.fn}{.col}")) 
-  }else{l <- tibble( min_col = 0, max_col = ncol(img),
-                     min_row = 0, max_row = nrow(img))}
-  img_ <- img[l$min_row:l$max_row,l$min_col:l$max_col]
+  }
+  else{l <- tibble( min_col = 0, max_col = ncol(img),
+                    min_row = 0, max_row = nrow(img))}
   
-  # get grob and save as list
-  grob <- grid::rasterGrob(img_, width=unit(1,"npc"), height=unit(1,"npc"))
-  images_tibble <- tibble(sample=factor(sampleid), grob=list(grob))
+  ## Spatial image:
+  if (!(img_alpha = 0)) {
+    # set image alpha:
+    im <- spe@images[[sampleid]]@image
+    spe@images[[sampleid]]@image <- matrix(
+      rgb(im[,,1],im[,,2],im[,,3], im[4,,]* img_alpha), nrow=dim(im)[1]) 
+    
+    img <- GetImage(spe, image = sampleid, mode = "raster")
+    img_ <- img[l$min_row:l$max_row,l$min_col:l$max_col]
+    
+    # get grob and save as list
+    grob <- grid::rasterGrob(img_, width=unit(1,"npc"), height=unit(1,"npc"))
+    images_tibble <- tibble(sample=factor(sampleid), grob=list(grob))
+    
+    spatial_image <- geom_spatial(data=images_tibble, aes(grob=grob), x=0.5, y=0.5)}
+  else{spatial_image <- NULL}
+  
+  ## Spatial annotation:
+  if(sp_annot){
+    spatial_annotation <- geom_path(
+      data=spe@tools[[sampleid]], 
+      show.legend = FALSE, size = .3,
+      aes(x=x, y=y, colour=colour, 
+          group=interaction(elem_idx)))
+  }
+  else{spatial_annotation <- NULL}
   
   p <- ggplot()+
-    geom_spatial(data=images_tibble, aes(grob=grob), x=0.5, y=0.5)+
     geom_point(data=df, aes(x=imagecol,y=imagerow,fill=.data[[geneid]]),
-               shape = 21, #colour = "black",
-               size = point_size, stroke = 0.5, alpha = alpha)+
-    geom_path(data = spe@tools[[sampleid]], aes(x=x, y=y, group = interaction(elem_idx)))+
-    colour_pallet +
-    coord_cartesian(expand=FALSE )+
-    
+               shape = 21, colour = "transparent",
+               size = point_size, stroke = 0.5, alpha = alpha) +
+    spatial_image +
+    spatial_annotation + 
+    scale_color_manual(values=c("#808080", "transparent")) + colour_pallet +
+    coord_cartesian(expand=FALSE ) + theme(l) +
     xlim(l$min_col,l$max_col) +
     ylim(l$max_row,l$min_row)
   
@@ -145,49 +162,6 @@ vis_gene_p <- function(
           axis.text = element_blank(),
           axis.ticks = element_blank())
   return(p)
-}
-
-#################
-# PATH FUNCTIONS #
-#################
-c_curve<-function(start, c1, c2, end, n=100) {
-  c1 <- start+c1
-  c2 <- start+c2
-  end <- start+end
-  sapply((0:n)/n, function(i) {
-    q1 <- start+i*(c1-start)
-    q2 <- c1+i*(c2-c1)
-    q3 <- c2+i*(end-c2)
-    r1 <- q1+i*(q2-q1)
-    r2 <- q2+i*(q3-q2)
-    r1+i*(r2-r1)
-  })
-}
-# https://stackoverflow.com/questions/46221703/converting-svg-to-ggplot
-parse_svg_path <- function(path) {
-  parts <- regmatches(path, gregexpr("[A-Za-z]|,|-?[0-9.]+", path, perl=T))[[1]] 
-  parts <- parts[parts!=","]
-  vals <- suppressWarnings(as.numeric(parts))
-  i <- 1
-  points <- matrix(ncol=0, nrow=2)
-  while(i < length(parts)) {
-    if (parts[i]=="M") {
-      points <- cbind(points, c(vals[i+1], vals[i+2]))
-      i <- i+3
-    } else if (parts[i]=="l") {
-      cpoints <- c_curve(
-        points[, ncol(points)],
-        c(vals[i+1], vals[i+2]),
-        c(vals[i+3], vals[i+4]),
-        c(vals[i+5], vals[i+6])
-      )
-      points <- cbind(points, cpoints)
-      i <- i+7
-    } else {
-      stop(paste("unrecognized command", parts[i]))
-    }
-  }
-  points
 }
 
 #################
